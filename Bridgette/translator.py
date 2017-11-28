fe78ed1940edac0817e06f46d5fb26696cdfed62
Bridgette Drummond <BeeDrummond@bridgettes-air.dhcp.nd.edu>
2017-11-28 16:44:00 -0500

    some pseudocode for translator

---
+++ Bridgette/translator.py
@@ -0,0 +1,25 @@
+##Translate nucelotides to amino acid sequence
+
+#load packages
+
+#load map
+map = open("codonmap.txt", "r")
+
+#load nucleotide sequence to translate as read only
+
+#create document as write only to save results to
+
+
+#define start of sequence
+    #variable1 = some kind of search function looking for "ATG'
+
+#define end of sequence
+    #variable2 = some kind of search function looking for 'TAA' in variable1
+    
+#CDS = sequence between variable 1 and variable 2 points
+
+#for loop for 3 nucleotide letters in CDS
+    #replace with amino acid letter
+    #append to document
+    #go to next 3 letters
+    
