550e13d5ced6cbda25e7fa2a3bfa42195f3dba26
janicelove <jlove5@nd.edu>
2017-11-28 22:11:00 -0500

    need to get protein sequences for each transcript from NCBI

---
+++ split_transcripts/janice_protein.sh
@@ -0,0 +1,7 @@
+#Date: Nov. 28, 2017
+#Author: Janice Love 
+
+##Storing command line scipts for protein database part of bioinformatics project
+
+cat topHits | cut -d "," -f 4 > topAcc #takes accession numbers from column 4 of topHits table from part 1
+
