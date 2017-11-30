fbd39790f5d6975b5419f6fdb661ed31b6248259
janicelove <jlove5@nd.edu>
2017-11-30 16:49:48 -0500

    still broken

--- janice_translate.py
+++ janice_translate.py
@@ -2,6 +2,9 @@
 #Author: Janice Love
 #file for translating amino acids of provided sequences into proteinSequences
 
+#import packages
+import re 
+
 #open data files 
 ctrl_1 = open ("control1.fasta", 'r')
 ctrl_2 = open ("control2.fasta", 'r')
@@ -10,9 +13,10 @@ exp_2 = open ("obese2.fasta", 'r')
 
 codons = open ("codonmap.txt", 'r')
 
-#create a dictionary for codon map 
+##create a dictionary for codon map 
 codonmap_dict = {}
 
+#for loop to read codonmap.txt into dictionary 
 #reverse col 0 and 1 to create full dictionary. (compared to dict code from tut8)
 for line in codons:
     line = line.strip()
@@ -24,17 +28,33 @@ for line in codons:
         codonmap_dict [cols[1]] = cols[0]
 
 stop_codons = ['TAA', 'TGA', 'TAG']
+stop = re.compile(r'(TAA | TGA | TAG)')
 
 ##function to find first ATG, trim the sequence 
-def trimSeq (sequence, stop_codons):
-    start = sequence.find('ATG')
-    sequencestart = sequence[int(start):]
-    stop = sequencestart.find('TAA' or 'TAG' or 'TGA')
-    cds = str(sequencestart[:int(stop)+3])
+#use regex instead?
 
+def trimSeq (sequence, stop):
+    #search for first occurence of 'ATG'
+    start = sequence.find('ATG')
+    
+    #Set sequence to start there
+    sequencestart = sequence[int(start):]
+     
+    
+    #Search for first stop codon 
+    end = stop.match(sequencestart)
+    
+    
+    #stop = sequencestart.find('TAA' or 'TAG' or 'TGA')
+    cds = str(sequencestart[:(end)])
+    
     # Take sequence from the first start codon
     trimmed_sequence = sequence[start:]
+    print trimmed_sequence 
 
+    return trimmed_sequence
+
+def codon_split (trimmed_sequence)
     # Split it into triplets
     codons = [trimmed_sequence[i:i+3] for i in range(0, len(trimmed_sequence), 3)]
     #print(len(codons))
@@ -48,11 +68,11 @@ for line in ctrl_1:
     if ">" in line:
         outfile.write(line + "\n")
     else:
-        line = sequence 
-        trimSeq (sequence, stop_codons)
-    
+        sequence = line
 
 
+
+trimSeq (sequence, stop)
 
 
 codons.close()        
