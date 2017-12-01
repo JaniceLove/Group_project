147cb7d72ce26936ca0b20a2c2d17c509df2ad28
janicelove <jlove5@nd.edu>
2017-11-30 22:52:25 -0500

    GOT PROTEIN SEQUENCESgit add -A finally

--- janice_translate.py
+++ janice_translate.py
@@ -3,8 +3,12 @@
 #file for translating amino acids of provided sequences into proteinSequences
 
 #import packages
-import re 
+import numpy as np 
 import pandas as pd 
+import re
+from sys import argv
+script, filename = argv
+from itertools import takewhile
 
 #open data files 
 ctrl_1 = open ("control1.fasta", 'r')
@@ -32,35 +36,49 @@ for line in codons:
 #stop = re.compile(r'(TAA | TGA | TAG)')
 
 ##function to read fasta file 
-#def read_fasta (file):
-
-
-sequence = []
-for line in ctrl_1:
-    line = line.strip()
-    if ">" in line: 
-        sequence.append (line)
-
 ##function to find first start codon 
 sequence = sequenceStart.find('ATG')
-
 ##function to search for stop codon: tried a variety of these, but can't get them to work. Why?? 
 stop = sequenceStart.match(r'TAA | TAG | TGA') #re.compile first, then search as object? OR re.search? 
-
-
 stop = re.compile (r'TAA|TAG|TGA')
 end = stop.match(sequenceStart)
-
-#function to define cds (coding sequence)
+##function to define cds (coding sequence)
 cds = str(sequenceStart[:(end)])
-
 ##translate cds to protein sequence using codonmap_dict 
 #split cds sequence into codons 
 codons = [cds[i:i+3] for i in range(0, len(trimmed_sequence), 3)]
-
 ##save the translated protein sequences to files 
 
+##-----------------------------------------------------------------------------------------
+def translate_dna(sequence, codonmap_dict, stop_codons = ('TAA', 'TGA', 'TAG')):
+    #find first start codon 
+    start = sequence.find('ATG')
+    
+    #trim sequence starting at first start codon 
+    trimmed_sequence = sequence[start:]
+    
+    #split sequence into codons 
+    codons = [trimmed_sequence[i:i+3] for i in range(0, len(trimmed_sequence), 3)]
+    #print(len(codons))
+    #print(trimmed_sequence)
+    #print(codons)
 
+    coding_sequence = takewhile(lambda x: x not in stop_codons and len(x) == 3 , codons)
+    protein_sequence = ''.join([codonmap_dict[codon] for codon in coding_sequence])
+
+    return "{0}_".format(protein_sequence)
+#------------------------------------------------------------------------------------------
+
+outfile = open ("ctrl_1_protein", 'w')
+for line in open("control1.fasta", 'r'):
+	line = line.strip()
+	if ">" in line: 
+	    header = line 
+	    outfile.write(header + "\n")
+	else:
+	    sequence = line 
+	    x = translate_dna (sequence, codonmap_dict, stop_codons = ('TAA', 'TGA', 'TAG')) 
+	    outfile.write(x + "\n")
 
 codons.close()        
 ctrl_1.close()
